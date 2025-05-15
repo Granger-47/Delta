05d69e7505676e1793fbd109cc47082d1c43b853
Granger-47 <sakethniitt@gmail.com>
2025-05-15 23:44:41 +0530

    Delete Hacker mode directory

--- Hacker mode/responsive.js
+++
@@ -1,9 +0,0 @@
-function syncCenteredWidth() {
-    const container = document.getElementById("container");
-    const centered = document.getElementById("centered");
-    centered.style.width = `${container.offsetWidth}px`;
-  }
-  
-  window.addEventListener("load", syncCenteredWidth);
-  window.addEventListener("resize", syncCenteredWidth);
-  
